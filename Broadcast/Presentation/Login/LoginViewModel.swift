bd90b22a6d17635b5a9927a2d019086673ab7674
Peter Suwara <psuwara@realityworks.com.au>
2020-11-23 18:05:29 +0300

    Initial login view model

--- Broadcast/Presentation/Login/LoginViewModel.swift
+++ Broadcast/Presentation/Login/LoginViewModel.swift
@@ -6,11 +6,26 @@
 //
 
 import Foundation
-
+import RxCocoa
+import RxSwift
 
 class LoginViewModel : ViewModel {
+    private let authenticationUseCase: AuthenticationUseCase
+    
+    let usernamePublisher = BehaviorRelay<String?>(value: nil)
+    let passwordPublisher = BehaviorRelay<String?>(value: nil)
+    let isLoadingActivityVisible = BehaviorRelay<Bool>(value: false)
+    let isLoginEnabled: Observable<Bool>
     
     init(dependencies: Dependencies = .standard) {
+        self.authenticationUseCase = dependencies.authenticationUseCase
+        
+        isLoginEnabled = Observable.combineLatest(usernamePublisher, passwordPublisher) { username, password in
+            guard let username = username,
+                  let password = password = 
+            return username.
+        }
+        
         super.init(stateController: dependencies.stateController)
     }
 }
@@ -20,9 +35,10 @@ extension LoginViewModel {
     struct Dependencies {
         
         let stateController: StateController
+        let authenticationUseCase: AuthenticationUseCase
         
         static let standard = Dependencies(
-            stateController: StateController.standard)
-        
+            stateController: Domain.standard.stateController,
+            authenticationUseCase: Domain.standard.useCases.authenticationUseCase)
     }
 }
