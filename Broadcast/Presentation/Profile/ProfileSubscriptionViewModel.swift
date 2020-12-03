bfa41b02e6a62420aaf0861e1dea4e51beebe8f0
Peter Suwara <psuwara@realityworks.com.au>
2020-12-03 06:20:08 +0300

    Added profile subscription controller/model

---
+++ Broadcast/Presentation/Profile/ProfileSubscriptionViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileDetailViewModel.swift
+//  Broadcast
+//
+//  Created by Piotr Suwara on 3/12/20.
+//
+
+import Foundation
+
+class ProfileSubscriptionViewModel : ViewModel {
+    
+    init(dependencies: Dependencies = .standard) {
+        super.init(stateController: dependencies.stateController)
+        
+    }
+    
+}
+
+/// NewPostViewModel dependencies component
+extension ProfileSubscriptionViewModel {
+    struct Dependencies {
+        
+        let stateController: StateController
+        
+        static let standard = Dependencies(
+            stateController: StateController.standard)
+    }
+}
+
