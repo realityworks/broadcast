a831eb07bb53d3787fe2792ea5e7ae4de477259d
Peter Suwara <psuwara@realityworks.com.au>
2020-12-07 06:35:11 +0300

    WIP Trailer Cell

--- Broadcast/Presentation/Profile/Views/ProfileTrailerTableViewCell.swift
+++ Broadcast/Presentation/Profile/Views/ProfileTrailerTableViewCell.swift
@@ -11,17 +11,29 @@ import RxSwift
 class ProfileTrailerTableViewCell: UITableViewCell {
     static let identifier: String = "ProfileTrailerTableViewCell"
     static let cellHeight: CGFloat = 300
+    
+    let verticalStack = UIStackView()
+    let videoPlayerView = VideoPlayerView()
+    let selectButton = UIButton.standard(withTitle: LocalizedString.select)
 
     override init(style: UITableViewCell.CellStyle,
                   reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        videoPlayerView.layer.cornerRadius = 10
+        videoPlayerView.clipsToBounds = true
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func configure(text: String) {
-
+    func configure(thumbnailUrl: URL?) {
+        if let thumbnailUrl = thumbnailUrl {
+            verticalStack.addArrangedSubview(videoPlayerView)
+            verticalStack.addArrangedSubview(selectButton)
+        } else {
+            
+        }
     }
 }
