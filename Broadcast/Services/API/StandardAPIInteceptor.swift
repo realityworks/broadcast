88dcb99532455a8cec5bf4b9a5d3767d01526017
Peter Suwara <psuwara@realityworks.com.au>
2020-12-14 20:07:16 +0300

    Added Request Inteceptor with Retry override

--- Broadcast/Services/API/StandardAPIInteceptor.swift
+++ Broadcast/Services/API/StandardAPIInteceptor.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import Alamofire
+import RxAlamofire
+
+class StandardAPIInteceptor : Interceptor {
+    override func retry(_ request: Request,
+               for session: Session,
+               dueTo error: Error,
+               completion: @escaping (RetryResult) -> Void) {
+        guard let response = request.task?.response as? HTTPURLResponse else {
+            completion(.doNotRetryWithError(error))
+            return
+        }
+        
+        print ("Status Code: \(response.statusCode)")
+        
+        completion(.doNotRetryWithError(error))
+        return
+    }
+}
