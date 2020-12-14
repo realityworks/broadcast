ed218ca75e79f10ae8a76eb70484c4ac683e4a67
Peter Suwara <psuwara@realityworks.com.au>
2020-12-14 09:37:35 +0300

    Initial upload service with publish subject

--- Broadcast/Services/Upload/StandardUploadService.swift
+++ Broadcast/Services/Upload/StandardUploadService.swift
@@ -9,29 +9,48 @@ import Foundation
 import RxSwift
 import RxCocoa
 import RxAlamofire
+import Alamofire
 
 class StandardUploadService : UploadService {
     
+    let baseUrl: URL
+    let uploadSession: Session
+    let schedulers: Schedulers
+
+    
     var media: Media?
     var content: NewPost?
+    var uploadProgressPublishSubject: PublishSubject<UploadProgress>?
     
     private func createContentForUpload() {
         
     }
     
-    private func createMediaUploadUrl(){
+    private func createMediaUploadUrl() {
         
     }
     
-    func uploadMedia()
-    func uploadContent()
-    func publishContent()
+    private func uploadMedia() {
+        
+    }
     
-    func upload(media: Media, content: NewPost) -> Observable<(Data?, RxProgress)> {
-        // TODO
+    private func uploadContent() {
+        
+    }
+    
+    private func publishContent() {
+        
+    }
+    
+    func upload(media: Media, content: NewPost) -> Observable<UploadProgress> {
+        self.media = media
+        self.content = content
+        
+        uploadProgressPublishSubject = PublishSubject<UploadProgress>()
+        
         let endpointUrl = ""
         
-        return request(.get, endpointUrl)
+        let mediaRequest = request(.get, endpointUrl)
             .flatMap { request -> Observable<(Data?, RxProgress)> in
                 let dataPart = request.rx
                     .data()
@@ -41,17 +60,41 @@ class StandardUploadService : UploadService {
                 return Observable.combineLatest(dataPart, progressPart) { ($0, $1) }
             }
             .observeOn(MainScheduler.instance)
+        
+        return uploadProgressPublishSubject!
+            .asObservable()
     }
     
     init(dependencies: Dependencies = .standard) {
+        self.media = nil
+        self.content = nil
+        self.uploadProgressPublishSubject = nil
         
+        self.baseUrl = dependencies.baseUrl
+        self.schedulers = dependencies.schedulers
+        self.uploadSession = Session.default
     }
 }
 
-// MARK: Dependencies
+// MARK: Instance Dependencies
 
 extension StandardUploadService {
+    
+    static let standard = {
+        StandardUploadService()
+    }()
+}
+
+
+// MARK: Dependencies
+extension StandardUploadService {
     struct Dependencies {
-        static let standard = Dependencies()
+        
+        let schedulers: Schedulers
+        let baseUrl: URL
+        
+        static let standard = Dependencies(
+            schedulers: Schedulers.standard,
+            baseUrl: Configuration.apiServiceURL)
     }
 }
