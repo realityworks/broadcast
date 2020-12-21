4f9a391404137fd271de552eff5bba42ed2efbfd
Peter Suwara <psuwara@realityworks.com.au>
2020-12-21 07:21:01 +0300

    Removed processing state from Post media content

--- Broadcast/Domain/State/Post.swift
+++ Broadcast/Domain/State/Post.swift
@@ -46,56 +46,38 @@ struct Post: Equatable, Codable {
         self.caption = caption
         self.comments = comments
         self.lockers = lockers
+        self.finishedProcessing = finishedProcessing
         self.created = created
         self.media = media
     }
     
     init(from decoder: Decoder) throws {
-        #warning("TODO : Needs to be fully decoded")
         let container = try decoder.container(keyedBy: PostCodingKeys.self)
         self.id = try container.decode(String.self, forKey: .id)
         self.title = try container.decode(String.self, forKey: .title)
         self.caption = try container.decode(String.self, forKey: .title)
+        self.finishedProcessing = try container.decode(Bool.self, forKey: .finishedProcessing)
         
+        #warning("TODO - Waiting on Bryan for endpoint")
         self.comments = 0
         self.lockers = 0
-        self.created = (try container.decode(String.self, forKey: .created))
+        
+        let iso8601String = try container.decode(String.self, forKey: .created)
+        self.created = Date(iso8601String: iso8601String) ?? Date()
         
         self.media = try container.decode(PostMedia.self, forKey: .media)
     }
     
     struct PostMedia : Equatable, Codable {
+        
+        enum ContentType: String, Codable {
+            case image = "image/jpeg"
+            case video = "video/mp4"
+        }
+        
         let id: MediaID
-        let url: String
-        
+        let thumbnailUrl: String?
+        let contentUrl: String
+        let contentType: ContentType
     }
-    
-    struct PostVideo : Equatable, Codable {
-        enum VideoState : String, Equatable, Codable {
-            case available
-            case encoding
-            case failed
-            case new
-            case uploadComplete
-        }
-        
-        enum VideoProcessingStatus : String, Equatable, Codable {
-            case downloading
-            case error
-            case inProgress
-            case none
-            case pendingUpload
-            case queued
-            case ready
-        }
-        
-        let videoState: VideoState
-        let videoProcessingStatus: VideoProcessingStatus
-        let postVideoUrl: String
-    }
-    
-    struct PostImage : Equatable, Codable {
-        let postImageUrl: String
-    }
-    
 }
