7821261bac9094bd4e73cea15dbd6b4eba315b3d
Peter Suwara <psuwara@realityworks.com.au>
2020-12-25 06:54:46 +0300

    New colours

--- Broadcast/Presentation/Styling/UIColor+Styling.swift
+++ Broadcast/Presentation/Styling/UIColor+Styling.swift
@@ -9,6 +9,8 @@ import UIKit
 
 extension UIColor {
     static let primaryRed = UIColor(hex: "#FD0A4C")
+    static let primaryBlack = UIColor(hex: "#14141A")
+    static let primaryGrey = UIColor(hex: "#4C576F")
     static let largeTitle = UIColor(hex: "#040F23")
 }
 
