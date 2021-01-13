9842d51efc613369d4b2871f892916c95d2efee5
Peter Suwara <psuwara@realityworks.com.au>
2021-01-13 15:45:02 +0300

    Moved the post content into a scroll view underneath the delete button, so the delete button is always visible

--- Broadcast/Presentation/Post Detail/PostDetailViewController.swift
+++ Broadcast/Presentation/Post Detail/PostDetailViewController.swift
@@ -11,8 +11,11 @@ class PostDetailViewController: ViewController {
     private let viewModel: PostDetailViewModel = PostDetailViewModel()
     
     // MARK: - UI Components
-    let verticalStackView = UIStackView()
+    //let verticalStackView = UIStackView()
+    let deleteButtonContainer = UIView()
     let deleteButton = UIButton.textDestructive(withTitle: LocalizedString.deletePost)
+    
+    let scrollView = UIScrollView()
     var postSummaryView: PostSummaryView!
     
     override func viewDidLoad() {
@@ -30,20 +33,40 @@ class PostDetailViewController: ViewController {
     private func configureViews() {
         view.backgroundColor = UIColor.secondaryWhite
         postSummaryView.backgroundColor = .white
+        deleteButtonContainer.backgroundColor = .clear
         
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
         // Configure the view settings
-        verticalStackView.axis = .vertical
-        verticalStackView.distribution = .equalSpacing
+        //verticalStackView.axis = .vertical
+        //verticalStackView.distribution = .equalSpacing
     }
     
     private func configureLayout() {
         // Layout the subviews
-        view.addSubview(verticalStackView)
-        verticalStackView.addArrangedSubview(postSummaryView)
-        verticalStackView.addSpace(40)
-        verticalStackView.addArrangedSubview(deleteButton)
+//        view.addSubview(verticalStackView)
+//        verticalStackView.addArrangedSubview(postSummaryView)
+//        verticalStackView.addSpace(40)
+//        verticalStackView.addArrangedSubview(deleteButton)
+//
+//        verticalStackView.edgesToSuperview(excluding: [.bottom], usingSafeArea: true)
         
-        verticalStackView.edgesToSuperview(excluding: [.bottom], usingSafeArea: true)
+        view.addSubview(scrollView)
+        view.addSubview(deleteButtonContainer)
+        
+        scrollView.edgesToSuperview(excluding: [.bottom], usingSafeArea: true)
+        scrollView.bottomToTop(of: deleteButtonContainer)
+                
+        deleteButtonContainer.edgesToSuperview(excluding: [.top, .bottom], usingSafeArea: true)
+        deleteButtonContainer.bottomToSuperview(usingSafeArea: true)
+        deleteButtonContainer.height(80)
+               
+        scrollView.addSubview(postSummaryView)
+        postSummaryView.topToSuperview()
+        postSummaryView.bottomToSuperview()
+        postSummaryView.widthToSuperview()
+        
+        deleteButtonContainer.addSubview(deleteButton)
+        deleteButton.centerInSuperview()
     }
     
     private func configureBindings() {
