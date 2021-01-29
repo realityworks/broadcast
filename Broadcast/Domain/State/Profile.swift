180104a7ff388251bae019faedc36d2f8def512d
Peter Suwara <psuwara@realityworks.com.au>
2021-01-29 06:14:13 +0300

    Removed testing surface

--- Broadcast/Domain/State/Profile.swift
+++ Broadcast/Domain/State/Profile.swift
@@ -25,7 +25,7 @@ struct Profile : Equatable, Codable {
     let profileImageUrl: String?
     let trailerThumbnailUrl: String?
     let trailerVideoUrl: String?
-    let isTrailerProcessed: Bool = true
+    let isTrailerProcessed: Bool
     
     struct StripeAccount : Equatable, Codable {
         let productId: String?
