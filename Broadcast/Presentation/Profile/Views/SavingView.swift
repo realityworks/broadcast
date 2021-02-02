f1f72df29e4edfc66d0c4a4f5c5b5f4a61102b2f
Peter Suwara <psuwara@realityworks.com.au>
2021-02-02 09:27:00 +0300

    WIP Saving view

---
+++ Broadcast/Presentation/Profile/Views/SavingView.swift
@@ -0,0 +1,22 @@
+//
+//  SavingView.swift
+//  Broadcast
+//
+//  Created by Piotr Suwara on 2/2/21.
+//
+
+import UIKit
+
+class SavingView: UIView {
+    
+    private let titleLabel = UILabel.largeTitle(LocalizedString.saving, textColor: <#T##UIColor#>)
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
