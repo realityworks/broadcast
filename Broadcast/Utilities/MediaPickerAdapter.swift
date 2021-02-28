6cd280ddc6d163083e3870bd102588f06bd4abbb
Peter Suwara <psuwara@realityworks.com.au>
2021-02-28 13:31:58 +0300

    Switch to newer .photoLibrary option for media selection. Added check for authorization on PhotoLibrary

--- Broadcast/Utilities/MediaPickerAdapter.swift
+++ Broadcast/Utilities/MediaPickerAdapter.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import AVFoundation
+import Photos
 
 typealias PickerTag = Int
 
@@ -42,8 +43,33 @@ extension MediaPickerAdapter where Self: UIViewController {
     
     /// Function to create an image picker controller that allows media selection
     func mediaFromLibrary(forTag tag: PickerTag = 0) {
+        let authStatus = PHPhotoLibrary.authorizationStatus()
+        if authStatus == PHAuthorizationStatus.denied {
+            showDeniedAlert(title: "Unable to access the photo library",
+                            message: "To enable access, go to Settings > Privacy > Photos and turn on Photos access for this app.")
+        } else if authStatus == PHAuthorizationStatus.notDetermined {
+            PHPhotoLibrary.requestAuthorization { authStatus in
+                DispatchQueue.main.async {
+                    if #available(iOS 14, *) {
+                        /// Limited only available in iOS 14
+                        if authStatus == .authorized || authStatus == .limited {
+                            self.showImagePickerForLibrary(forTag: tag)
+                        }
+                    } else {
+                        if authStatus == .authorized {
+                            self.showImagePickerForLibrary(forTag: tag)
+                        }
+                    }
+                }
+            }
+        } else {
+            showImagePickerForLibrary(forTag: tag)
+        }
+    }
+    
+    func showImagePickerForLibrary(forTag tag: PickerTag = 0) {
         let selectedPicker = picker(forTag: tag)
-        selectedPicker.sourceType = .savedPhotosAlbum
+        selectedPicker.sourceType = .photoLibrary
         selectedPicker.mediaTypes = supportedAlbumModes(forTag: tag)
         selectedPicker.modalPresentationStyle = UIModalPresentationStyle.popover
         present(selectedPicker, animated: true, completion: nil)
@@ -53,41 +79,11 @@ extension MediaPickerAdapter where Self: UIViewController {
     func mediaFromCamera(forTag tag: PickerTag = 0) {
         let authStatus = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
         if authStatus == AVAuthorizationStatus.denied {
-
-            // Denied access to camera, alert the user.
-
-            // The user has previously denied access. Remind the user that we need camera access to be useful.
-
-            let alert = UIAlertController(title: "Unable to access the Camera",
-                                          message: "To enable access, go to Settings > Privacy > Camera and turn on Camera access for this app.",
-                                          preferredStyle: UIAlertController.Style.alert)
-
-            let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-            alert.addAction(okAction)
-
-            let settingsAction = UIAlertAction(title: "Settings", style: .default, handler: { _ in
-                // Take the user to Settings app to possibly change permission.
-                guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else { return }
-
-                if UIApplication.shared.canOpenURL(settingsUrl) {
-                    UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
-                        // Finished opening URL
-                    })
-                }
-            })
-
-            alert.addAction(settingsAction)
-            present(alert, animated: true, completion: nil)
-        }
-
-        else if (authStatus == AVAuthorizationStatus.notDetermined) {
+            showDeniedAlert(title: "Unable to access the Camera",
+                            message: "To enable access, go to Settings > Privacy > Camera and turn on Camera access for this app.")
+        } else if (authStatus == AVAuthorizationStatus.notDetermined) {
             // The user has not yet been presented with the option to grant access to the camera hardware.
             // Ask for permission.
-            //
-            // (Note: you can test for this case by deleting the app on the device, if already installed).
-            // (Note: we need a usage description in our Info.plist to request access.
-            //
-
             AVCaptureDevice.requestAccess(for: AVMediaType.video, completionHandler: { (granted) in
                 if granted {
                     DispatchQueue.main.async {
@@ -138,6 +134,33 @@ extension MediaPickerAdapter where Self: UIViewController {
             })
         alert.addAction(actLibrary)
 
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: Internal functions
+    
+    private func showDeniedAlert(title: String, message: String) {
+        // Denied access to camera, alert the user.
+        // The user has previously denied access. Remind the user that we need camera access to be useful.
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: UIAlertController.Style.alert)
+
+        let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(okAction)
+
+        let settingsAction = UIAlertAction(title: "Settings", style: .default, handler: { _ in
+            // Take the user to Settings app to possibly change permission.
+            guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else { return }
+
+            if UIApplication.shared.canOpenURL(settingsUrl) {
+                UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                    // Finished opening URL
+                })
+            }
+        })
+
+        alert.addAction(settingsAction)
         present(alert, animated: true, completion: nil)
     }
 }
